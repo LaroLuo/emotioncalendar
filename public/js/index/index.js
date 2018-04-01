8cdf4758aeeaa087d9847d57bf79d9eb92afca43
Youcheny <youcheny@usc.edu>
2018-03-31 17:35:43 -0700

    sign in integrated

--- public/js/index/index.js
+++ public/js/index/index.js
@@ -1,8 +1,4 @@
 var month = getMonth();
 var day = getDay();
 var daySuffix = getDaySuffix();
-var hours = getHours();
-var minutes = getMinutes();
-document.getElementById("ti").innerHTML += hours + " : " + minutes;
 document.getElementById("da").innerHTML += month + " " + day + daySuffix;
-startTime();
